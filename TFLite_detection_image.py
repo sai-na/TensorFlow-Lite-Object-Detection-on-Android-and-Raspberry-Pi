14c59d22baed5ef3010328e1f275b3f240684eed
sainath378 <92171156+sai-na@users.noreply.github.com>
2023-04-29 17:19:32 +0530

    Update TFLite_detection_image.py

--- TFLite_detection_image.py
+++ TFLite_detection_image.py
@@ -2,9 +2,9 @@
 #
 # Author: Evan Juras
 # Date: 11/11/22
-# Description: 
-# This program uses a TensorFlow Lite object detection model to perform object 
-# detection on an image or a folder full of images. It draws boxes and scores 
+# Description:
+# This program uses a TensorFlow Lite object detection model to perform object
+# detection on an image or a folder full of images. It draws boxes and scores
 # around the objects of interest in each image.
 #
 # This code is based off the TensorFlow Lite image classification example at:
@@ -54,8 +54,8 @@ LABELMAP_NAME = args.labels
 min_conf_threshold = float(args.threshold)
 use_TPU = args.edgetpu
 
-save_results = args.save_results # Defaults to False
-show_results = args.noshow_results # Defaults to True
+save_results = args.save_results  # Defaults to False
+show_results = args.noshow_results  # Defaults to True
 
 IM_NAME = args.image
 IM_DIR = args.imagedir
@@ -94,28 +94,29 @@ CWD_PATH = os.getcwd()
 
 # Define path to images and grab all image filenames
 if IM_DIR:
-    PATH_TO_IMAGES = os.path.join(CWD_PATH,IM_DIR)
-    images = glob.glob(PATH_TO_IMAGES + '/*.jpg') + glob.glob(PATH_TO_IMAGES + '/*.png') + glob.glob(PATH_TO_IMAGES + '/*.bmp')
+    PATH_TO_IMAGES = os.path.join(CWD_PATH, IM_DIR)
+    images = glob.glob(PATH_TO_IMAGES + '/*.jpg') + glob.glob(PATH_TO_IMAGES +
+                                                              '/*.png') + glob.glob(PATH_TO_IMAGES + '/*.bmp')
     if save_results:
         RESULTS_DIR = IM_DIR + '_results'
 
 elif IM_NAME:
-    PATH_TO_IMAGES = os.path.join(CWD_PATH,IM_NAME)
+    PATH_TO_IMAGES = os.path.join(CWD_PATH, IM_NAME)
     images = glob.glob(PATH_TO_IMAGES)
     if save_results:
         RESULTS_DIR = 'results'
 
 # Create results directory if user wants to save results
 if save_results:
-    RESULTS_PATH = os.path.join(CWD_PATH,RESULTS_DIR)
+    RESULTS_PATH = os.path.join(CWD_PATH, RESULTS_DIR)
     if not os.path.exists(RESULTS_PATH):
         os.makedirs(RESULTS_PATH)
 
 # Path to .tflite file, which contains the model that is used for object detection
-PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,GRAPH_NAME)
+PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, GRAPH_NAME)
 
 # Path to label map file
-PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,LABELMAP_NAME)
+PATH_TO_LABELS = os.path.join(CWD_PATH, MODEL_NAME, LABELMAP_NAME)
 
 # Load the label map
 with open(PATH_TO_LABELS, 'r') as f:
@@ -153,9 +154,9 @@ input_std = 127.5
 # because outputs are ordered differently for TF2 and TF1 models
 outname = output_details[0]['name']
 
-if ('StatefulPartitionedCall' in outname): # This is a TF2 model
+if ('StatefulPartitionedCall' in outname):  # This is a TF2 model
     boxes_idx, classes_idx, scores_idx = 1, 3, 0
-else: # This is a TF1 model
+else:  # This is a TF1 model
     boxes_idx, classes_idx, scores_idx = 0, 1, 2
 
 # Loop over every image and perform detection
@@ -164,7 +165,7 @@ for image_path in images:
     # Load image and resize to expected shape [1xHxWx3]
     image = cv2.imread(image_path)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    imH, imW, _ = image.shape 
+    imH, imW, _ = image.shape
     image_resized = cv2.resize(image_rgb, (width, height))
     input_data = np.expand_dims(image_resized, axis=0)
 
@@ -173,13 +174,16 @@ for image_path in images:
         input_data = (np.float32(input_data) - input_mean) / input_std
 
     # Perform the actual detection by running the model with the image as input
-    interpreter.set_tensor(input_details[0]['index'],input_data)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
     interpreter.invoke()
 
     # Retrieve detection results
-    boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
-    classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0] # Class index of detected objects
-    scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0] # Confidence of detected objects
+    boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[
+        0]  # Bounding box coordinates of detected objects
+    classes = interpreter.get_tensor(output_details[classes_idx]['index'])[
+        0]  # Class index of detected objects
+    scores = interpreter.get_tensor(output_details[scores_idx]['index'])[
+        0]  # Confidence of detected objects
 
     detections = []
 
@@ -189,27 +193,37 @@ for image_path in images:
 
             # Get bounding box coordinates and draw box
             # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-            ymin = int(max(1,(boxes[i][0] * imH)))
-            xmin = int(max(1,(boxes[i][1] * imW)))
-            ymax = int(min(imH,(boxes[i][2] * imH)))
-            xmax = int(min(imW,(boxes[i][3] * imW)))
-            
-            cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+            ymin = int(max(1, (boxes[i][0] * imH)))
+            xmin = int(max(1, (boxes[i][1] * imW)))
+            ymax = int(min(imH, (boxes[i][2] * imH)))
+            xmax = int(min(imW, (boxes[i][3] * imW)))
+
+            cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
 
             # Draw label
-            object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
-            label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
-            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
-            label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
-            cv2.rectangle(image, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
-            cv2.putText(image, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+            # Look up object name from "labels" array using class index
+            object_name = labels[int(classes[i])]
+            if object_name == "dog":
 
-            detections.append([object_name, scores[i], xmin, ymin, xmax, ymax])
+                label = '%s: %d%%' % (object_name, int(
+                    scores[i]*100))  # Example: 'person: 72%'
+                labelSize, baseLine = cv2.getTextSize(
+                    label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
+                # Make sure not to draw label too close to top of window
+                label_ymin = max(ymin, labelSize[1] + 10)
+                # Draw white box to put label text in
+                cv2.rectangle(image, (xmin, label_ymin-labelSize[1]-10), (
+                    xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)
+                cv2.putText(image, label, (xmin, label_ymin-7),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)  # Draw label text
+
+                detections.append(
+                    [object_name, scores[i], xmin, ymin, xmax, ymax])
 
     # All the results have been drawn on the image, now display the image
     if show_results:
         cv2.imshow('Object detector', image)
-        
+
         # Press any key to continue to next image, or press 'q' to quit
         if cv2.waitKey(0) == ord('q'):
             break
@@ -219,20 +233,21 @@ for image_path in images:
 
         # Get filenames and paths
         image_fn = os.path.basename(image_path)
-        image_savepath = os.path.join(CWD_PATH,RESULTS_DIR,image_fn)
-        
+        image_savepath = os.path.join(CWD_PATH, RESULTS_DIR, image_fn)
+
         base_fn, ext = os.path.splitext(image_fn)
-        txt_result_fn = base_fn +'.txt'
-        txt_savepath = os.path.join(CWD_PATH,RESULTS_DIR,txt_result_fn)
+        txt_result_fn = base_fn + '.txt'
+        txt_savepath = os.path.join(CWD_PATH, RESULTS_DIR, txt_result_fn)
 
         # Save image
         cv2.imwrite(image_savepath, image)
 
         # Write results to text file
         # (Using format defined by https://github.com/Cartucho/mAP, which will make it easy to calculate mAP)
-        with open(txt_savepath,'w') as f:
+        with open(txt_savepath, 'w') as f:
             for detection in detections:
-                f.write('%s %.4f %d %d %d %d\n' % (detection[0], detection[1], detection[2], detection[3], detection[4], detection[5]))
+                f.write('%s %.4f %d %d %d %d\n' % (
+                    detection[0], detection[1], detection[2], detection[3], detection[4], detection[5]))
 
 # Clean up
 cv2.destroyAllWindows()
