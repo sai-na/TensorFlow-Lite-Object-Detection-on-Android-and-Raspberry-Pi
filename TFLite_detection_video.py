8c37ae037bcac92e8ef7c1e3777d325402005e17
sainath378 <92171156+sai-na@users.noreply.github.com>
2023-04-30 23:55:10 +0530

    if not pygame.mixer.music.get_busy():

--- TFLite_detection_video.py
+++ TFLite_detection_video.py
@@ -167,18 +167,20 @@ while(video.isOpened()):
             object_name = labels[int(classes[i])]
             if object_name == "dog":
                 # Play the dog sound
-                os.system("aplay lion.wav")
-                label = '%s: %d%%' % (object_name, int(
-                    scores[i]*100))  # Example: 'person: 72%'
-                labelSize, baseLine = cv2.getTextSize(
-                    label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
-                # Make sure not to draw label too close to top of window
-                label_ymin = max(ymin, labelSize[1] + 10)
-                # Draw white box to put label text in
-                cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (
-                    xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)
-                cv2.putText(frame, label, (xmin, label_ymin-7),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)  # Draw label text
+                   # Check if Pygame mixer is currently busy playing a sound
+                if not pygame.mixer.music.get_busy():
+                    os.system("aplay lion.wav")
+                    label = '%s: %d%%' % (object_name, int(
+                        scores[i]*100))  # Example: 'person: 72%'
+                    labelSize, baseLine = cv2.getTextSize(
+                        label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
+                    # Make sure not to draw label too close to top of window
+                    label_ymin = max(ymin, labelSize[1] + 10)
+                    # Draw white box to put label text in
+                    cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (
+                        xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)
+                    cv2.putText(frame, label, (xmin, label_ymin-7),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)  # Draw label text
 
     # All the results have been drawn on the frame, so it's time to display it.
     cv2.imshow('Object detector', frame)
